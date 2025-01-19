33dc7cd866ba2c028d6f9028e21cba5ed293a5d9
Luka Vilagoš <luka.vilagos@gmail.com>
2025-01-19 23:26:05 +0100

    Fixed bug

--- sql/sastojak/select-by-recept.sql
+++ sql/sastojak/select-by-recept.sql
@@ -1,1 +1,12 @@
-CALL SastojciZaRecept(%s);
+SELECT
+    s.id AS ID,
+    s.naziv AS Sastojak,
+    rs.kolicina AS PotrebnaKolicina,
+    s.kolicina_tip AS KolicinaTip,
+    s.slika AS Slika
+FROM
+    recept_sastojak rs
+JOIN
+    sastojak s ON rs.sastojak_id = s.id
+WHERE
+    rs.recept_id = %s;
